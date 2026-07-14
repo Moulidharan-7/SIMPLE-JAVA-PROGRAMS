8ed1e06d24a711dd21448e3675c956311be73f91
Moulidharan-7 <moulidharanbn7@gmail.com>
2026-07-14 21:57:42 +0530

    linear_list

---
+++ linear_list_add_first.java
@@ -0,0 +1,23 @@
+public class linear_list_add_first {
+        public static int linear(int[]arr,int add){
+        if(arr[0]== -1) return 0 ;
+        int loop=0;
+        while(loop<arr.length && arr[loop]!=-1){
+            loop++;
+        }
+        
+        if(arr.length==loop) return 0;
+        
+        for(int j=loop;j>0;j--){
+            arr[j]=arr[j-1];
+        }
+        arr[0]=add;
+        loop++;
+        
+        return loop;
+    }
+    public static void main(String[] args) {
+        int[] ar={1,2,2,3,4,5,6,-1,-1};
+        System.out.println(linear(ar, 5));
+    }
+}
